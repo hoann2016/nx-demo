b1d1a0fd5c126fe85df5742068d1601504cd0ff5
Hoa Nguyen <hoann.2016@gmail.com>
2024-04-29 19:46:51 +0700

    before fix bug cannot run npx nx serve shop

--- src/app/shop/src/app/app.routes.ts
+++ src/app/shop/src/app/app.routes.ts
@@ -1,6 +1,11 @@
+import { NxWelcomeComponent } from './nx-welcome.component';
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: NxWelcomeComponent,
+  },
   {
     path: 'product',
     loadComponent: () =>
